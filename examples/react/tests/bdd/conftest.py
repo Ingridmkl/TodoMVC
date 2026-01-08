46afa83fe781e5f54bddf71aaa5254ba774fb17f
Ingridmkl <ingrid.moukala@eleve.isep.fr>
2026-01-09 00:42:33 +0100

    Add github actions pipeline for jest and selenium- IM

--- examples/react/tests/bdd/conftest.py
+++ examples/react/tests/bdd/conftest.py
@@ -1,11 +1,24 @@
 import pytest
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def driver():
+    # Setup Chrome Options
+    chrome_options = Options()
+    
+    # Check if running in GitHub Actions (CI environment)
+    if os.environ.get('CI') == 'true':
+        chrome_options.add_argument("--headless") # No GUI
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--window-size=1920,1080")
+    
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    
     yield driver
     driver.quit()
